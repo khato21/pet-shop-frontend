4d5f62bd62b9cc1875d492c2a679939db507fa5e
khato21 <x.papelashvili@yahoo.com>
2026-08-11 21:03:01 +0400

    Initial frontend setup

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+
+      "/currency": {
+        target: "https://bankofgeorgia.ge",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/currency/, "/api"),
+      },
+    },
+  },
+});
